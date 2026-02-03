5fe732e93aed7932c11ac1b31434a0959cdf0478
Abdullah al mamun <61002570+stranger-1-1-13@users.noreply.github.com>
2026-02-03 22:38:43 +0600

    finished

--- tem.js
+++ tem.js
@@ -4,9 +4,31 @@ Sugar is sweet,
 And so are you.`;
 console.log(poem);
 
-constgetTax = (amount, taxRate = 0.05) => {
-  return amount + (amount * taxRate);
+
+
+const device = {name: 'phone', price: 500};
+const {price}= device;
+console.log(price);   
+
+const employee = {id: 101, 
+    name: 'Alice', 
+    salary: 70000,
+    experience: 5
+};
+const keys = Object.keys(employee);
+const values = Object.values(employee);
+console.log(keys);
+console.log(values);
+
+
+for(const key of keys){
+    console.log(key, employee[key]);
 }
 
-const product = { name: "Laptop", price: 1000 };
-console.log(getTax(product.price));
+employee.department = 'IT';
+Object.freeze(employee);
+employee.salary = 80000;
+console.log(employee.salary);
+
+const entries = Object.entries(employee);
+console.log(entries);

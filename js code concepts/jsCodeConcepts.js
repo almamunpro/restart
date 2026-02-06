54cfbb1150b6fe52a83963c3b9a065c57674a52c
Abdullah al mamun <61002570+stranger-1-1-13@users.noreply.github.com>
2026-02-06 23:37:26 +0600

    module 3

---
+++ js code concepts/jsCodeConcepts.js
@@ -0,0 +1,27 @@
+// JS is a dynamically typed language, 
+// which means that variables can hold values of any type 
+// and the type can change at runtime. In the example below, 
+// we declare a variable `data` and assign it different types of values:
+
+let data = 40;
+data = 'Hello World';
+data = true;
+data = {name: 'John', age: 30};
+data = [1, 2, 3, 4, 5];
+data = null;
+data = undefined;
+console.log(typeof data);
+
+// premetive data types
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. Null
+// 5. Undefined
+// 6. Symbol
+
+// usually we don't use undifined, it is used by JS when we declare a variable without assigning a value to it.
+// non-premetive data types
+// 1. Object
+// 2. Array
+// 3. Function

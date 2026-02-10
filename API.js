e8cfb3f86d3039972b3239edede7545c995ab4b7
Abdullah al mamun <61002570+stranger-1-1-13@users.noreply.github.com>
2026-02-11 00:33:30 +0600

    module 2

---
+++ API.js
@@ -0,0 +1,9 @@
+console.log("Exploring the API.js file...");
+// This file is intended to demonstrate basic API interactions using JavaScript.
+const person = {
+    name: "Alice",
+    age: 30,
+    city: "Wonderland"
+};
+console.log(`Name: ${person.name}, Age: ${person.age}, City: ${person.city}`);
+
